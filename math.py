ae687ca35ecae1c55364230f4b0fc12957face3f
vinodkkumar99 <74234677+vinodkkumar99@users.noreply.github.com>
2021-07-08 14:28:37 +0530

    Initial commit of math.py

---
+++ math.py
@@ -0,0 +1,13 @@
+def add(x.y)
+return x+y
+def mul(x,y)
+return x*y
+def div(x,y)
+if y==0:           #on bug987
+return divid _o_by _error
+else:
+return x/y
+
+#square implementation
+def square(x):
+pass
